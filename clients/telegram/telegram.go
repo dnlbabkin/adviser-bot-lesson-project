5ebe84ea7d0f9ed15ebbb7ace9aab5ec1359a02c
dnlbabkin <danilbabkin57@gmail.com>
2024-02-06 20:55:46 +0300

    init

---
+++ clients/telegram/telegram.go
@@ -0,0 +1,96 @@
+package telegram
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"read-adviser-bot/lib/e"
+	"strconv"
+)
+
+const (
+	getUpdatesMethod  = "getUpdates"
+	sendMessageMethod = "sendMessage"
+)
+
+type Client struct {
+	host     string
+	basePath string
+	client   http.Client
+}
+
+func (c *Client) Updates(offset, limit int) ([]Update, error) {
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+
+	data, err := c.doRequest(q, getUpdatesMethod)
+	if err != nil {
+		return nil, err
+	}
+
+	var res UpdatesResponse
+
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+
+	return res.Result, nil
+}
+
+func (c *Client) SendMessages(chatId int, text string) error {
+	q := url.Values{}
+	q.Add("chat_id", strconv.Itoa(chatId))
+	q.Add("text", text)
+
+	_, err := c.doRequest(q, sendMessageMethod)
+	if err != nil {
+		return e.Wrap("can't send message", err)
+	}
+
+	return nil
+}
+
+func (c *Client) doRequest(query url.Values, method string) (data []byte, err error) {
+	defer func() { err = e.WrapIfErr("can't do request", err) }()
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func New(host, token string) *Client {
+	return &Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
---
+++ events/telegram/commands.go
@@ -0,0 +1,103 @@
+package telegram
+
+import (
+	"errors"
+	"log"
+	"net/url"
+	"read-adviser-bot/lib/e"
+	"read-adviser-bot/storage"
+	"strings"
+)
+
+const (
+	RndCmd   = "/rnd"
+	HelpCmd  = "/help"
+	StartCmd = "/start"
+)
+
+func (p *Processor) doCmd(text string, chatID int, username string) error {
+	text = strings.TrimSpace(text)
+
+	log.Printf("got new command '%s' from '%s'", text, username)
+
+	if isAddCmd(text) {
+		return p.savePage(chatID, text, username)
+
+	}
+
+	switch text {
+	case RndCmd:
+		return p.sendRandom(chatID, username)
+	case HelpCmd:
+		return p.sendHelper(chatID)
+	case StartCmd:
+		return p.sendHello(chatID)
+	default:
+		return p.tg.SendMessages(chatID, msgUnknownCommand)
+	}
+}
+
+func (p *Processor) savePage(chatID int, pageURL string, username string) (err error) {
+	defer func() { err = e.WrapIfErr("can't do command: save page", err) }()
+
+	page := &storage.Page{
+		URL:      pageURL,
+		UserName: username,
+	}
+
+	isExists, err := p.storage.IsExists(page)
+	if err != nil {
+		return err
+	}
+
+	if isExists {
+		return p.tg.SendMessages(chatID, msgAlreadyExists)
+	}
+
+	if err := p.storage.Save(page); err != nil {
+		return err
+	}
+
+	if err := p.tg.SendMessages(chatID, msgSaved); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Processor) sendRandom(chatID int, username string) (err error) {
+	defer func() { err = e.WrapIfErr("can't do command: send random", err) }()
+
+	page, err := p.storage.PickRandom(username)
+	if err != nil && !errors.Is(err, storage.ErrNoSavedPage) {
+		return err
+	}
+
+	if errors.Is(err, storage.ErrNoSavedPage) {
+		return p.tg.SendMessages(chatID, msgNoSavedPage)
+	}
+
+	if err := p.tg.SendMessages(chatID, page.URL); err != nil {
+		return err
+	}
+
+	return p.storage.Remove(page)
+}
+
+func (p *Processor) sendHelper(chatID int) error {
+	return p.tg.SendMessages(chatID, msgHelp)
+}
+
+func (p *Processor) sendHello(chatID int) error {
+	return p.tg.SendMessages(chatID, msgHello)
+}
+
+func isAddCmd(text string) bool {
+	return isURL(text)
+}
+
+func isURL(text string) bool {
+	u, err := url.Parse(text)
+
+	return err == nil && u.Host != ""
+}
